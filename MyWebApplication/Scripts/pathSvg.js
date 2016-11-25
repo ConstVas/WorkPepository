24bf28ca119fda28f802f3ce7b8a3a6207341466
VasilevKA <VasilevKA@quarta.su>
2016-11-25 18:11:09 +0300

    Path created

---
+++ MyWebApplication/Scripts/pathSvg.js
@@ -0,0 +1,15 @@
+﻿var height = 100;
+    width = 1100;
+    var data = [];
+    for (i = 0; i < 1000; i++) {
+        var rnd = Math.floor(Math.random()*100)
+        data.push({ x: i, y: rnd })
+    }
+
+var line = d3.svg.line()
+            .x(function (d) { return d.x; })
+            .y(function (d) { return d.y; });
+var svg = d3.select("#path").append('svg');
+svg.attr("height", height)
+    .attr("width", width);
+svg.append("path").attr("d", line(data));
